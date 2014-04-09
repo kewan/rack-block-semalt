e0b8e791d29e1fe4879a58463402252952cfb22e
Michael Dippery <michael@monkey-robot.com>
2014-04-08 20:03:43 -0700

    Initial commit to repo

---
+++ lib/rack/block/semalt.rb
@@ -0,0 +1,9 @@
+require "rack/block/semalt/version"
+
+module Rack
+  module Block
+    module Semalt
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rack/block/semalt/version.rb
@@ -0,0 +1,7 @@
+module Rack
+  module Block
+    module Semalt
+      VERSION = "0.0.1"
+    end
+  end
+end
